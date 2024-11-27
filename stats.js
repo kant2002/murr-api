69d6533abd801c44671e75602b6963bccf35f108
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-27 11:21:27 +0500

    Add shoppping availability

--- stats.js
+++ stats.js
@@ -5,6 +5,8 @@ const url = process.argv[2];
 const token = process.argv[3];
 const name = process.argv[4];
 const api = new MurrApi(url, token);
+const shopInfo = await api.getShopInfo();
+console.log(`Shopping availability: ${shopInfo.shopping.uiMessage}`)
 const todayStats = await api.getTopSellers(true, 50);
 for (let i = 0; i < todayStats.top.length; i++) {
     const shop = todayStats.top[i];
